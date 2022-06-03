ed6ce7425d717a87cccf1a3275c0ed4b49d88d92
DaMVgt <dmonterroso098@outlook.com>
2022-06-03 17:17:10 -0600

    ✅ Add: Entidades y parametros

--- Entities/Jornada.cs
+++ Entities/Jornada.cs
@@ -6,5 +6,6 @@ namespace WebApiKalum.Entities
         public string Prefijo { get; set; }
         public string Descripcion { get; set; }
         public virtual List<Aspirante> Aspirantes { get; set; }
+        public virtual List<Inscripcion> Inscripciones { get; set; }
     }
 }
