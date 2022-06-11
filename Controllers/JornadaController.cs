a5e438127ec66add96af9e120bcdaf5e23a6a51e
DaMVgt <dmonterroso098@outlook.com>
2022-06-11 16:20:36 -0600

    ✅ Metodos update, delete e insert

--- Controllers/JornadaController.cs
+++ Controllers/JornadaController.cs
@@ -38,5 +38,46 @@ namespace WebApiKalum.Controllers
             }
             return Ok(jornada);
         }
+        [HttpPost]
+        public async Task<ActionResult<Jornada>> PostJornda([FromBody] Jornada value)
+        {
+            Logger.LogDebug("Iniciando el proceso de creacion de una jornada");
+            value.JornadaId = Guid.NewGuid().ToString().ToUpper();
+            await DbContext.Jornada.AddAsync(value);
+            await DbContext.SaveChangesAsync();
+            Logger.LogInformation("Se ha creado una jornada con el id: ");
+            return new CreatedAtRouteResult("GetJornada", new { id = value.JornadaId }, value);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Jornada>> DeleteJornada(string id)
+        {
+            Logger.LogDebug("Iniciando el proceso de eliminacion de una jornada");
+            Jornada jornada = await DbContext.Jornada.FirstOrDefaultAsync(j => j.JornadaId == id);
+            if (jornada == null){
+                Logger.LogWarning($"No existe una jornada con el id: {id}");
+                return NotFound();
+            }
+            else{
+                DbContext.Jornada.Remove(jornada);
+                await DbContext.SaveChangesAsync();
+                Logger.LogInformation($"Se ha eliminado la jornada con el id: {id}");
+                return jornada;
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutJornada(string id, [FromBody] Jornada value)
+        {
+            Logger.LogDebug($"Iniciando el proceso de actualizacion de la jornada con el id: {id}");
+            Jornada jornada = await DbContext.Jornada.FirstOrDefaultAsync(j => j.JornadaId == id);
+            if(jornada == null){
+                Logger.LogWarning($"No existe una jornada con el id: {id}");
+                return BadRequest();
+            }
+            jornada.Prefijo = value.Prefijo;
+            jornada.Descripcion = value.Descripcion;
+            await DbContext.SaveChangesAsync();
+            Logger.LogInformation($"Se ha actualizado la jornada con el id: {id}");
+            return NoContent();
+        }
     }
 }
--- Entities/Jornada.cs
+++ Entities/Jornada.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiKalum.Entities
 {
     public class Jornada
     {
+        [Required]
         public string JornadaId { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 1, ErrorMessage = "La cantidad minima de caracteres es {2} y la maxima es {1} para el campo {0}")]
         public string Prefijo { get; set; }
+        [Required]
+        [StringLength(128, MinimumLength = 5, ErrorMessage = "La cantidad minima de caracteres es {2} y la maxima es {1} para el campo {0}")]
         public string Descripcion { get; set; }
         public virtual List<Aspirante> Aspirantes { get; set; }
         public virtual List<Inscripcion> Inscripciones { get; set; }
