e46ca6a81deaea843078167f87fc9a1b8f43abfe
DaMVgt <dmonterroso098@outlook.com>
2022-06-12 16:15:20 -0600

    update branch

--- Entities/ExamenAdmision.cs
+++ Entities/ExamenAdmision.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiKalum.Entities
 {
     public class ExamenAdmision
     {
+        [Required]
         public string ExamenId { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime FechaExamen { get; set; }
         public virtual List<Aspirante> Aspirantes { get; set; }
     }
