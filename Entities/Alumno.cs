1fcd83b80978e198f6b4de5520997d47c769a7ab
DaMVgt <dmonterroso098@outlook.com>
2022-06-12 15:20:45 -0600

    ✅ Finish: Feature/Alumnos

--- Entities/Alumno.cs
+++ Entities/Alumno.cs
@@ -1,12 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiKalum.Entities
 {
     public class Alumno
     {
+        [Required]
+        [StringLength(8, MinimumLength = 8)]
         public string Carne { get; set; }
+        [Required]
+        [StringLength(128, MinimumLength = 5, ErrorMessage = "La cantidad minima de caracteres es {2} y la maxima es {1} para el campo {0}")]
         public string Apellidos { get; set; }
+        [Required]
+        [StringLength(128, MinimumLength = 5, ErrorMessage = "La cantidad minima de caracteres es {2} y la maxima es {1} para el campo {0}")]
         public string Nombres { get; set; }
+        [Required]
+        [StringLength(128, MinimumLength = 5, ErrorMessage = "La cantidad minima de caracteres es {2} y la maxima es {1} para el campo {0}")]
         public string Direccion { get; set; }
+        [Required]
+        [StringLength(64, MinimumLength = 8)]
         public string Telefono { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public virtual List<Inscripcion> Inscripciones { get; set; }
         public virtual List<CuentaXCobrar> CuentaXCobrar { get; set; }
