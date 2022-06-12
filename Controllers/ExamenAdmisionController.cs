212da689b8cb6789cd855a4e6d0804313ecae15d
DaMVgt <dmonterroso098@outlook.com>
2022-06-12 15:59:51 -0600

    ✅ Finish: feature/examen-admision

--- Controllers/ExamenAdmisionController.cs
+++ Controllers/ExamenAdmisionController.cs
@@ -21,7 +21,8 @@ namespace WebApiKalum.Controller
             List<ExamenAdmision> examenAdmision = null;
             Logger.LogDebug("Iniciando proceso de consulta en la base de datos");
             examenAdmision = await DbContext.ExamenAdmision.Include(e => e.Aspirantes).ToListAsync();
-            if (examenAdmision == null || examenAdmision.Count == 0){
+            if (examenAdmision == null || examenAdmision.Count == 0)
+            {
                 Logger.LogWarning("No existen examenes en la base de datos");
                 return new NoContentResult();
             }
@@ -33,12 +34,60 @@ namespace WebApiKalum.Controller
         {
             Logger.LogDebug("Iniciando el proceso de busca con el id: " + id);
             var examenAdmision = await DbContext.ExamenAdmision.Include(e => e.Aspirantes).FirstOrDefaultAsync(e => e.ExamenId == id);
-            if (examenAdmision == null){
+            if (examenAdmision == null)
+            {
                 Logger.LogWarning("No existe un examen con el id: " + id);
                 return new NoContentResult();
             }
             Logger.LogInformation("Se ejecuto la peticion de forma exitosa");
             return Ok(examenAdmision);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ExamenAdmision>> PostExamenAdmision([FromBody] ExamenAdmision value)
+        {
+            Logger.LogDebug("Iniciando el proceso de creacion de un examen");
+            value.ExamenId = Guid.NewGuid().ToString().ToUpper();
+            await DbContext.ExamenAdmision.AddAsync(value);
+            await DbContext.SaveChangesAsync();
+            Logger.LogInformation("Se creo el examen exitosamente");
+            return new CreatedAtRouteResult("GetExamenAdmision", new { id = value.ExamenId }, value);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ExamenAdmision>> DeleteExamenAdmision(string id)
+        {
+            Logger.LogDebug("Iniciando el proceso de eliminacion de un examen");
+            ExamenAdmision examenAdmision = await DbContext.ExamenAdmision.FirstOrDefaultAsync(e => e.ExamenId == id);
+            if (examenAdmision == null)
+            {
+                Logger.LogWarning($"No existe un examen con el id: {id}");
+                return NotFound();
+            }
+            else
+            {
+                DbContext.ExamenAdmision.Remove(examenAdmision);
+                await DbContext.SaveChangesAsync();
+                Logger.LogInformation($"Se elimino el examen con el id: {id}");
+                return examenAdmision;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutExamenAdmision(string id, [FromBody] ExamenAdmision value)
+        {
+            Logger.LogDebug($"Iniciando el proceso de actualizacion de un examen con el id: {id}");
+            ExamenAdmision examenAdmision = await DbContext.ExamenAdmision.FirstOrDefaultAsync(e => e.ExamenId == id);
+            if (examenAdmision == null)
+            {
+                Logger.LogWarning($"No existe un examen con el id: {id}");
+                return BadRequest();
+            }
+            examenAdmision.FechaExamen = value.FechaExamen;
+            DbContext.Entry(examenAdmision).State = EntityState.Modified;
+            await DbContext.SaveChangesAsync();
+            Logger.LogInformation($"Se actualizo el examen con el id: {id}");
+            return NoContent();
         }
     }
 }
