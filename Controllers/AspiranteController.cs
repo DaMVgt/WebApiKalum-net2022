b8429f6aee2c0f4df04c3177a5775a831b2d2f9c
DaMVgt <dmonterroso098@outlook.com>
2022-06-18 11:33:52 -0600

    ✅ Update validaciones

--- Controllers/AspiranteController.cs
+++ Controllers/AspiranteController.cs
@@ -21,7 +21,8 @@ namespace WebApiKalum.Controllers
             List<Aspirante> aspirantes = null;
             Logger.LogDebug("Iniciando proceso de consulta de aspirantes en la base de datos");
             aspirantes = await DbContext.Aspirante.Include(a => a.ResultadoExamenAdmision).Include(a => a.InscripcionPago).ToListAsync();
-            if (aspirantes == null || aspirantes.Count == 0){
+            if (aspirantes == null || aspirantes.Count == 0)
+            {
                 Logger.LogWarning("No existen aspirantes en la base de datos");
                 return new NoContentResult();
             }
@@ -33,12 +34,40 @@ namespace WebApiKalum.Controllers
         {
             Logger.LogDebug("Iniciando el proceso de busca con el id: " + id);
             var aspirante = await DbContext.Aspirante.Include(a => a.ResultadoExamenAdmision).Include(a => a.InscripcionPago).FirstOrDefaultAsync(a => a.NoExpediente == id);
-            if (aspirante == null){
+            if (aspirante == null)
+            {
                 Logger.LogWarning("No existe un aspirante con el id: " + id);
                 return new NoContentResult();
             }
             Logger.LogInformation("Se ejecuto la peticion de forma exitosa");
             return Ok(aspirante);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Aspirante>> PostAspirante([FromBody] Aspirante value)
+        {
+            Logger.LogDebug("Iniciando el proceso de creacion de un aspirante");
+            CarreraTecnica carreraTecnica = await DbContext.CarreraTecnica.FirstOrDefaultAsync(ct => ct.CarreraId == value.CarreraId);
+            if (carreraTecnica == null)
+            {
+                Logger.LogInformation($"No existe una carrera tecnica con el id: {value.CarreraId}");
+                return BadRequest();
+            }
+            Jornada jornada = await DbContext.Jornada.FirstOrDefaultAsync(j => j.JornadaId == value.JornadaId);
+            if (jornada == null)
+            {
+                Logger.LogInformation($"No existe una jornada con el id: {value.JornadaId}");
+                return BadRequest();
+            }
+            ExamenAdmision examenAdmision = await DbContext.ExamenAdmision.FirstOrDefaultAsync(e => e.ExamenId == value.ExamenId);
+            if (examenAdmision == null)
+            {
+                Logger.LogInformation($"No existe un examen con el id: {value.ExamenId}");
+                return BadRequest();
+            }
+            await DbContext.Aspirante.AddAsync(value);
+            await DbContext.SaveChangesAsync();
+            Logger.LogInformation($"Se creo un aspirante con el id: {value.NoExpediente}");
+            return Ok(value);
         }
     }
 }
