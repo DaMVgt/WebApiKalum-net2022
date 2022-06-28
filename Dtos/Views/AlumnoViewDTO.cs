52cf99fe8591bad3c9726d9481ead52e316f72b9
DaMVgt <dmonterroso098@outlook.com>
2022-06-28 10:54:04 -0600

    ♻️Refactor ✅ Add: carpeta vies para DTOS,

---
+++ Dtos/Views/AlumnoViewDTO.cs
@@ -0,0 +1,10 @@
+namespace WebApiKalum.Dtos.Views
+{
+    public class AlumnoViewDTO
+    {
+        public string NombreCompleto { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
